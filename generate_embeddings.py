e0161ce1332f16337508272eba3bbf1ac31786cd
samarjeetkaur <27503567+SamarjeetKaur@users.noreply.github.com>
2023-04-13 00:01:56 +0530

    Save embeddings in a csv

--- generate_embeddings.py
+++ generate_embeddings.py
@@ -8,7 +8,10 @@ embedding_encoding = "cl100k_base"  # this the encoding for text-embedding-ada-0
 max_tokens = 8191  # the maximum for text-embedding-ada-002 is 8191
 
 # convert training data into a dataframe
-training_dataframe = pd.DataFrame(data,columns=["PII","Label"])
+training_dataframe = pd.DataFrame(data,columns=["pii","Label"])
 
-
+# apply embeddings transformation to the PII
 training_dataframe["embeddings"]= training_dataframe.PII.apply(lambda x : get_embedding(x,model))
+
+# save embeddings in a csv so that they can be re-used
+training_dataframe.to_csv("pii_embeddings.csv")
