6d5a2684f3e7920022b73cc797703e46c3a96407
samarjeetkaur <27503567+SamarjeetKaur@users.noreply.github.com>
2023-04-13 00:03:51 +0530

    Rename PII to pii

--- generate_embeddings.py
+++ generate_embeddings.py
@@ -11,7 +11,7 @@ max_tokens = 8191  # the maximum for text-embedding-ada-002 is 8191
 training_dataframe = pd.DataFrame(data,columns=["pii","Label"])
 
 # apply embeddings transformation to the PII
-training_dataframe["embeddings"]= training_dataframe.PII.apply(lambda x : get_embedding(x,model))
+training_dataframe["embeddings"]= training_dataframe.pii.apply(lambda x : get_embedding(x,model))
 
 # save embeddings in a csv so that they can be re-used
 training_dataframe.to_csv("pii_embeddings.csv")
